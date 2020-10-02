7bd942d853da05d818eb97e61123d4b4369f029a
Martin Tschechne <martints@tikgpu01.ee.ethz.ch>
2020-10-02 14:32:36 +0200

    Added No Online Logit Normalization Transformer Layer.

--- agents/networks/shared/transformer.py
+++ agents/networks/shared/transformer.py
@@ -217,6 +217,84 @@ class MTE(nn.Module):
         output = self.feedforward_layer_norm_residual(output, att_output)
         return output
 
+class NoOnlineLogitNormalizationLayer(nn.Module):
+    def __init__(self, vec_size, num_attention_heads, attention_head_size):
+        super(NoOnlineLogitNormalizationLayer, self).__init__()
+        self.vec_size = vec_size
+        self.num_attention_heads = num_attention_heads
+        self.attention_head_size = attention_head_size
+
+        self.query = LinearLayer(vec_size, vec_size)
+        self.key = LinearLayer(vec_size, vec_size)
+        self.value = LinearLayer(vec_size, vec_size)
+
+        self.dot_product_scale = 1.0 / math.sqrt(attention_head_size)
+
+    def split_heads_apart(self, x):
+        new_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
+        x = x.view(*new_shape)  # Changes the tensor shape without moving or copying data.
+        return x.permute(0, 2, 1, 3)  # Moves data to maintain the meaning of the reordered list of dimensions.
+
+    def concatenate_heads(self, x):
+        x = x.permute(0, 2, 1, 3).contiguous()
+        new_shape = x.size()[:-2] + (self.vec_size,)
+        return x.view(*new_shape)
+
+    def forward(self, tens):  # [B, N, C]
+        # B = batch size
+        # C = num vector columns
+        # H = num heads
+        # S = head size
+        # V = vec_size = H * S
+
+        # Project the input to get the QKV vectors.
+        queries = self.query(tens)  # [B, C, V]
+        keys = self.key(tens)       # [B, C, V]
+        values = self.value(tens)   # [B, C, V]
+
+        # Split the heads apart to operate on the head vectors.
+        split_queries = self.split_heads_apart(queries)  # [B, H, C, S]
+        split_keys = self.split_heads_apart(keys)        # [B, H, C, S]
+        split_values = self.split_heads_apart(values)    # [B, H, C, S]
+
+        # Take the dot product between each query and key to get the raw attention scores.
+        transposed_keys = split_keys.transpose(-1, -2)   # [B, H, S, C]
+        attention_scores = torch.matmul(split_queries, transposed_keys)  # [B, H, C, C]
+        attention_scores = attention_scores * self.dot_product_scale     # [B, H, C, C]
+
+        # Calculate the weighted value vectors.
+        weighted_values = torch.matmul(attention_scores, split_values)  # [B, H, C, S]
+
+        # Scale weighted value vectors by root of sequence length.
+        weighted_values /= math.sqrt(weighted_values.size()[-2]) # [B, H, C, S]
+
+        # Concatenate the heads for output.
+        output = self.concatenate_heads(weighted_values)  # [B, C, V]
+        return output
+
+class NON(nn.Module):
+    def __init__(self, vec_size, num_attention_heads, attention_head_size, hidden_layer_size):
+        super(NON, self).__init__()
+        self.attention = NoOnlineLogitNormalizationLayer(vec_size, num_attention_heads, attention_head_size)
+        self.attention_layer_norm_residual = LayerNormResidual(vec_size, vec_size, vec_size)
+
+        self.feedforward = LinearLayer(vec_size, hidden_layer_size)
+        self.feedforward_layer_norm = LayerNorm(hidden_layer_size)
+        self.feedforward_layer_norm_residual = LayerNormResidual(hidden_layer_size, vec_size, hidden_layer_size)
+
+    def forward(self, input):
+        # Attention phase.
+        att_output = self.attention(input)
+        att_output = F.gelu(att_output)
+        att_output = self.attention_layer_norm_residual(att_output, input)
+
+        # Feedforward phase.
+        output = self.feedforward(att_output)
+        output = self.feedforward_layer_norm(output)
+        output = F.gelu(output)
+        output = self.feedforward_layer_norm_residual(output, att_output)
+        return output
+
 class Transformer(nn.Module):
     def __init__(self, transformer_type, num_attention_heads, attention_head_size, num_layers, hidden_layer_size):
         super(Transformer, self).__init__()
@@ -231,6 +309,8 @@ class Transformer(nn.Module):
             TransformerLayerType = BERT
         elif transformer_type == "MTE":
             TransformerLayerType = MTE
+        elif transformer_type == "NON":
+            TransformerLayerType = NON
         else:
             TransformerLayerType = TransformerLayer
 
